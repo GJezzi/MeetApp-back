272805c3d132cfc0f7a2e8872778d9b971cec7e1
GJezzi <gjezzi@hotmail.com>
2019-10-29 15:57:22 -0300

    MongoDB Setup

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -13,16 +13,62 @@ class SubscriptionController {
       ],
     });
 
+    if (meetup.user_id === req.userId) {
+      return res
+        .status(400)
+        .json({ error: 'You cannot subscribe in a Meetup you created' });
+    }
+
     if (!meetup) {
       return res.status(400).json({ error: 'Meetup not found.' });
+    }
+
+    if (meetup.past) {
+      return res.status(400).json({
+        error:
+          'It is not possible to subscript in Meetup that has already happened',
+      });
+    }
+
+    const isSubscribed = await Subscription.findOne({
+      where: {
+        meetup_id: meetup.id,
+        user_id: req.userId,
+      },
+    });
+
+    if (isSubscribed) {
+      return res.status(400).json({
+        error: 'You are already subscribed to this meetup',
+      });
+    }
+
+    const sameTimeMeetup = await Subscription.findOne({
+      where: {
+        user_id: req.userId,
+      },
+      include: [
+        {
+          model: Meetup,
+          as: 'meetup',
+          where: {
+            date_time: meetup.date_time,
+          },
+        },
+      ],
+    });
+
+    if (sameTimeMeetup) {
+      return res.status(400).json({
+        error:
+          'There is another Meetup you are subscribed happening on the same time',
+      });
     }
 
     const subscription = await Subscription.create({
       user_id: req.userId,
       meetup_id: meetup.id,
     });
-    console.log('Meetup Id: ', meetup.id);
-    console.log('User Id: ', req.userId);
 
     return res.json(subscription);
   }
--- src/app/models/Meetup.js
+++ src/app/models/Meetup.js
@@ -1,4 +1,5 @@
 import { Sequelize, Model } from 'sequelize';
+import { isBefore } from 'date-fns';
 
 class Meetup extends Model {
   static init(sequelize) {
@@ -9,6 +10,12 @@ class Meetup extends Model {
         location: Sequelize.STRING,
         date_time: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date_time, Date.now());
+          },
+        },
       },
       {
         sequelize,
